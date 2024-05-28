0c6d9d7a03efa4147636478f631f687a79d1bfb4
Ma1akAs <1763611895@qq.com>
2024-05-28 11:18:23 +0800

    feat 完成服务器预览卡片组件

---
+++ monitor-frontend/src/views/main/Manage.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {defineComponent} from "vue";
+import PreviewCard from "@/component/PreviewCard.vue";
+
+defineComponent({
+  name: 'Manage'
+})
+</script>
+
+<template>
+  <div class="manage-main">
+    <div class="title">
+      <i class="fa-solid fa-server"></i>
+      管理主机列表
+    </div>
+    <div class="desc">
+      在这里管理你的主机实例
+    </div>
+    <el-divider style="margin: 10px 0"/>
+    <div class="card-list">
+      <preview-card />
+      <preview-card />
+      <preview-card />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.manage-main {
+  margin: 0 50px;
+
+  .title {
+    font-size: 22px;
+    font-weight: bold;
+  }
+
+  .desc {
+    font-size: 15px;
+    color: grey;
+  }
+}
+
+.card-list {
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+</style>
---
+++ monitor-frontend/src/views/main/Security.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {defineComponent} from "vue";
+
+defineComponent({
+  name: 'Security'
+})
+</script>
+
+<template>
+  <div>
+    安全
+  </div>
+</template>
+
+<style scoped>
+
+</style>
